2a84f6cc9b64e8649e2ea66f385116e344cb45b7
unknown <ehtisham57>
2024-02-06 02:07:40 +0500

    first commit~

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Headercomponent from "./components/header/Header";
+import Mainpage from "./components/mainpage/Mainpage";
+
+
+export default function Home() {
+  return (
+   <>
+   <Mainpage />
+   </>
+  );
+}
